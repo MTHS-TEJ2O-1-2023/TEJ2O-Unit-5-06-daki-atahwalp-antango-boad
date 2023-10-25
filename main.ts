a8ba88bdf7fe1f5067c12a81fea7569b0f677fbe
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-25 12:33:24 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Daki A.B
+ * Created on: Oct 2023
+ * This program uses sonar to measure distance 
 */
 
-basic.showString('Hello, World!')
+
