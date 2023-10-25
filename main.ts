3031e4edebf15e1646baf24a22dd341931868261
daki-atahwalp-antango-boad <144264104+daki-atahwalp-antango-boad@users.noreply.github.com>
2023-10-25 12:39:42 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,4 +5,21 @@
  * This program uses sonar to measure distance 
 */
 
+// variables
+let distanceFromObject: number = 0
 
+// on start up
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// measuring distance with sonar
+input.onButtonPressed(Button.A, function () {
+  basic.clearScreen()
+  distanceFromObject = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+  )
+  basic.showNumber(distanceFromObject)
+  basic.showIcon(IconNames.Happy)
+})
